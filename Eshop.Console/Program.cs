b27a3d1354f153af96c53630de10664244993fa5
Radoslav Romanyszyn <166935858+RadoslavRomanyszyn@users.noreply.github.com>
2025-06-30 23:47:55 +0200

    Initial Domain project with Product class

--- Eshop.Console/Program.cs
+++ Eshop.Console/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Eshop.Domain;
+
+var products = new List<Product>
+{
+    new Product("Notebook Dell 125", "Brand new from Dell", 649.99m),
+    new Product ("Mouse Logitech 77", "Ergonomic and precise", 14.50m),
+    new Product ("Keyboard Genius OP", "Your keys to success", 20.00m)
+};
+
+var product1 = products[0];
+product1.ChangeTitle("Notebook Dell 120");
+
+foreach (var product in products)
+{
+    Console.WriteLine($"{product.Title} . {product.Description} . {product.Price}");
+}
---
+++ Eshop.Domain/Product.cs
@@ -0,0 +1,41 @@
+﻿namespace Eshop.Domain
+{
+    public class Product
+    {
+        public Product(string title, string description, decimal price)
+        {
+            if (string.IsNullOrEmpty(title))
+            {
+                throw new ArgumentNullException("title");
+            }
+
+            if (string.IsNullOrEmpty(description))
+            {
+                throw new ArgumentNullException("description");
+            }
+
+            if (price < 0)
+            {
+                throw new ArgumentNullException("price");
+            }
+
+            Title = title;
+            Description = description;
+            Price = price;
+        }
+
+        public string Title { get; private set; }
+        public string Description { get; }
+        public decimal Price { get; }
+
+        public void ChangeTitle(string title)
+        {
+            if (string.IsNullOrEmpty(title))
+            {
+                throw new ArgumentNullException("title");
+            }
+
+            Title = title;
+        }
+    }
+}
