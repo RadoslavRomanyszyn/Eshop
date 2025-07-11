58ac4fbbcf01561b79f93cedcae11d0b33479487
Radoslav Romanyszyn <166935858+RadoslavRomanyszyn@users.noreply.github.com>
2025-07-11 19:00:05 +0200

    Create React client (Eshop.UI) and connect it to WebAPI

--- Eshop.WebApi/Controllers/ProductController.cs
+++ Eshop.WebApi/Controllers/ProductController.cs
@@ -7,18 +7,11 @@ namespace Eshop.WebApi.Controllers
     [Route("[controller]")]
     public class ProductController : ControllerBase
     {
-        private static List<Category> Categories = new List<Category>
-        {
-            new Category(1, "Notebooks", "Lorem ipsum"),
-            new Category(2, "Keyboards", "Lorem ipsum"),
-            new Category(3, "Mice", "Lorem ipsum")
-        };
-
         private static List<Product> Products = new List<Product>
         {
-            new Product(1, "Notebook Dell 125", "Brand new from Dell", 649.99m, Categories[0]),
-            new Product(2, "Mouse Logitech 77", "Ergonomic and precise", 14.50m, Categories[2]),
-            new Product(3, "Keyboard Genius OP", "Your keys to success", 20.00m, Categories[1])
+            new Product(1, "Notebook Dell 125", "Brand new from Dell", 649.99m, CategoryController.Categories[0]),
+            new Product(2, "Mouse Logitech 77", "Ergonomic and precise", 14.50m, CategoryController.Categories[2]),
+            new Product(3, "Keyboard Genius OP", "Your keys to success", 20.00m, CategoryController.Categories[1])
         };
 
         [HttpGet]
@@ -36,7 +29,7 @@ namespace Eshop.WebApi.Controllers
         [HttpPost]
         public Product CreateProduct(string title, string description, decimal price, int categoryId)
         {
-            var category = Categories.First(x => x.Id == categoryId);
+            var category = CategoryController.Categories.First(x => x.Id == categoryId);
             var newId = Products.Max(x => x.Id) + 1;
             var newProduct = new Product(newId, title, description, price, category);
             
@@ -55,7 +48,7 @@ namespace Eshop.WebApi.Controllers
         [HttpPut("{id}")]
         public Product UpdateProduct(int id, string title, string description, decimal price, int categoryId)
         {
-            var category = Categories.First(x => x.Id == categoryId);
+            var category = CategoryController.Categories.First(x => x.Id == categoryId);
             var productToBeUpdated = Products.First(x => x.Id == id);
             productToBeUpdated.Update(title, description, price, category);
 
