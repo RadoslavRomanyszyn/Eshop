942b08c0910a65da4239f3b0ecdcc4cc48da0c59
Radoslav Romanyszyn <166935858+RadoslavRomanyszyn@users.noreply.github.com>
2025-07-16 17:34:26 +0200

    Add product filtering to React App, basic layout

--- Eshop.UI/src/App.tsx
+++ Eshop.UI/src/App.tsx
@@ -1,37 +1,74 @@
 import { useEffect, useState } from "react";
 
 type CategoryType = {
-  id: number
-  title: string
-  description: string
+  id: number;
+  title: string;
+  description: string;
 };
 
-function App() {
+type ProductType = {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  category: CategoryType;
+};
+
+const App = () => {
   const [categories, setCategories] = useState<CategoryType[]>([]);
+  const [products, setProducts] = useState<ProductType[]>([]);
 
   useEffect(() => {
     const loadCategories = async () => {
-      const result = await (
+      const categoryResult = await (
         await fetch("https://localhost:7012/Category")
       ).json();
+      console.log("Categories loaded: ", categoryResult);
+      setCategories(categoryResult);
+    };
 
-      setCategories(result);
+    const loadProducts = async () => {
+      const productResult = await (
+        await fetch("https://localhost:7012/Product")
+      ).json();
+      console.log("Ptoducts loaded: ", productResult);
+      setProducts(productResult);
     };
 
     loadCategories();
+    loadProducts();
   }, []);
 
   return (
-    <ul>
-      {categories.map((x) => {
+    <div>
+      {categories.map((category) => {
+        const filteredProducts = products.filter(
+          (product) => product.category.id === category.id
+        );
+
         return (
-          <li key={x.id}>
-            {x.id} - {x.title}
-          </li>
+          <div key={category.id}>
+            <h2>
+              {category.id} - {category.title}
+            </h2>
+
+            <ul>
+              {filteredProducts.length ? (
+                filteredProducts.map((product) => (
+                  <li key={product.id}>
+                    {product.id} - {product.title} - {product.description} - {product.price} €
+                  </li>
+                ))
+              ) : (
+                <li>No products available for this category</li>
+              )}
+            </ul>
+            
+          </div>
         );
       })}
-    </ul>
+    </div>
   );
-}
+};
 
 export default App;
