24b7c28ce277230749632d2e35a7df0df21706dd
Radoslav Romanyszyn <radoslav.romanyszyn@gmail.com>
2026-09-04 18:13:48 +0200

    Add Web API Category integration tests

---
+++ tests/Eshop.WebApi.Tests/Features/Categories/DeleteCategoryTests.cs
@@ -0,0 +1,40 @@
+﻿using Eshop.Tests.Common.Mocks;
+using Eshop.WebApi.Exceptions;
+using Eshop.WebApi.Features.Categories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eshop.WebApi.Tests.Features.Categories
+{
+    public class DeleteCategoryTests : TestBase
+    {
+        [Test]
+        public async Task DeleteCategory_RemovesCategoryFromDb()
+        {
+            // arrange
+            var category = dbContext.Categories.Add(CategoryMocks.GetCategory1()).Entity;
+            await dbContext.SaveChangesAsync(CancellationToken.None);
+
+            var command = new DeleteCategory.Command(category.Id);
+            var handler = new DeleteCategory.Handler(dbContext);
+
+            // act
+            await handler.Handle(command, CancellationToken.None);
+
+            // assert
+            var remainingCategories = await dbContext.Categories.ToListAsync(CancellationToken.None);
+            Assert.That(remainingCategories, Is.Empty);
+        }
+
+        [Test]
+        public async Task DeleteCategory_WithInvalidCategoryId_ThrowsNotFoundException()
+        {
+            // arrange
+            const int invalidCategoryId = 999; // Assuming this ID does not exist in the database
+            var command = new DeleteCategory.Command(invalidCategoryId);
+            var handler = new DeleteCategory.Handler(dbContext);
+
+            // act / assert
+            Assert.ThrowsAsync<NotFoundException>(() => handler.Handle(command, CancellationToken.None));
+        }
+    }
+}
---
+++ tests/Eshop.WebApi.Tests/Features/Categories/GetCategoryTests.cs
@@ -0,0 +1,42 @@
+﻿using Eshop.Tests.Common.Mocks;
+using Eshop.WebApi.Exceptions;
+using Eshop.WebApi.Features.Categories;
+using Snapper;
+using Snapper.Attributes;
+
+namespace Eshop.WebApi.Tests.Features.Categories
+{
+    public class GetCategoryTests : TestBase
+    {
+        [Test]
+        // [UpdateSnapshots]
+        public async Task GetCategory_ReturnsCorrectDto()
+        {
+            // arrange
+            var category = CategoryMocks.GetCategory1();
+            dbContext.Categories.Add(category);
+            await dbContext.SaveChangesAsync(CancellationToken.None);
+
+            var query = new GetCategory.Query(category.Id);
+            var handler = new GetCategory.Handler(dbContext);
+
+            // act
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            // assert
+            result.ShouldMatchSnapshot();
+        }
+
+        [Test]
+        public async Task GetCategory_WithInvalidCategoryId_ThrowsNotFoundException()
+        {
+            // arrange
+            const int invalidCategoryId = 999; // Assuming this ID does not exist in the database
+            var query = new GetCategory.Query(invalidCategoryId);
+            var handler = new GetCategory.Handler(dbContext);
+
+            // act / assert
+            Assert.ThrowsAsync<NotFoundException>(() => handler.Handle(query, CancellationToken.None));
+        }
+    }
+}
